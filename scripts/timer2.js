f0ff022c6371a2b07dac5aeb91de7c220b8496f2
Joshua <joshuamdk6@gmail.com>
2021-01-21 23:36:24 +1100

    Fixing the Stuff i did last night

--- scripts/timer2.js
+++ scripts/timer2.js
@@ -14,6 +14,7 @@ let dateNamesFrom = {"sunday":0, "monday":1, "tuesday":2, "wednesday":3,
 let dateNamesTo = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
 	"Friday", "Saturday"]
 today.setHours(0,0,0,0);
+
 function daysFrom(date1, date2) {
 	let ndate1 = new Date(date1.getTime());
 	let ndate2 = new Date(date2.getTime());
@@ -26,6 +27,7 @@ function week() {
 	// this is fragile
 	// but the week a b system is fucked anyway ...
 }
+
 function timeTil() {
 	return (times[next].timeFrom + today.getTime()) - Date.now();
 }
@@ -58,7 +60,29 @@ function gen_table(json) {
 		tstr += "<tr><td id=\"time1\">";
 		if(v.room == "Sport"){
 			tstr += `<div class="timeSubtext">${k}: Sport</div>`
-		}else if(addDetails && v.room != "") {
+		}else if(!v.room){
+			switch(k){
+				case "P1": k = "Period 1"
+				break;
+				case "P2": k = "Period 2"
+				break;
+				case "P3": k = "Period 3"
+				break;
+				case "P4": k = "Period 4"
+				break;
+				case "P5": k = "Period 5"
+				break;
+				case "P6": k = "Period 6"
+				break;
+				case "P7": k = "Period 7"
+				break;
+				case "P8": k = "Period 8"
+				break;
+			}
+			if(!v.room && k !== "Recess" && k !== "Assembly" && k !=="Lunch" && k !== "End of Day")
+			tstr +=  `<div class="timeSubtext">${k} - ${v.startTime}</div>`
+
+		}else if(addDetails && v.room) {
 			tstr += `<div class="timeSubtext">${k}: ${v.subject} with ${v.teacher} - ${v.room}<div>`;
 		}
 		tstr += "</td><td id=\"time2\">";
@@ -68,9 +92,11 @@ function gen_table(json) {
 	table.innerHTML = tstr;
 	times.sort(function(a, b) {return a.timeFrom - b.timeFrom;});
 }
+
 function updateDay() {
 	document.getElementById("day").innerHTML = dateNamesTo[day()];
 }
+
 function moveDay(json) {
 	while (next == times.length) {
 		//this only occurs in the event that
@@ -94,14 +120,14 @@ function update(json) {
 		tt = timeTil();
 	}
 	periodCountdown.innerHTML = `${timeTilHMS()}`
-	period.innerHTML = `Next Event at ${json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].startTime}`;
 	if(json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].room === "Sport") periodInfo.innerHTML = "Sport";
 	else if(json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].room === "Lunch") periodInfo.innerHTML = "Lunch";
 	else if(json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].room === "Recess") periodInfo.innerHTML = "Recess";
 	else if(json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].room === "End of Day") periodInfo.innerHTML = "End of Day";
 	else if(json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].room === "Assembly") periodInfo.innerHTML = "Assembly";
-	else periodInfo.innerHTML = `${json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].subject} with ${json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].teacher}<br>in Room ${json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].room}`
+	else `${json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].name} in`;
 }
+
 let xhr = new XMLHttpRequest();
 xhr.responseType = 'json';
 xhr.open('GET', jsonPath + 'bellTimes.json', true);
