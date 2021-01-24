9869f5fe38446f1a9394265a30d3a1c0241e8a52
Joshua <63396516+theradongkid2@users.noreply.github.com>
2021-01-24 20:08:17 +1100

    Tomorrow and Mondays classes

--- scripts/timer.js
+++ scripts/timer.js
@@ -100,7 +100,10 @@ function update(json) {
 		moveDay(json);
 		tt = timeTil();
 	}
+	console.log(tt)
 	periodCountdown.innerHTML = `${timeTilHMS()}`
+	if(tt > 118200000)  document.getElementById("classTitle").innerHTML = "MONDAY'S CLASSES";
+	else if(tt > 31800000) document.getElementById("classTitle").innerHTML = "TOMORROW'S CLASSES";
 	period.innerHTML = `Next Event at ${json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].startTime}`;
 	if(json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].room === "Sport") periodInfo.innerHTML = "Sport";
 	else if(json.timetableData[dateNamesTo[day()].toLowerCase() + week()][times[next].periodName].room === "Lunch") periodInfo.innerHTML = "Lunch";
