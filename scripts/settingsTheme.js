4bfef4c3bb49214629768aa887f66c0d7a9c41be
Joshua <63396516+theradongkid2@users.noreply.github.com>
2021-05-31 22:31:16 +1000

    Adding Dark Mode (Update v2.1.0)

---
+++ scripts/settingsTheme.js
@@ -0,0 +1,23 @@
+// By Joshua Koh
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'default');
+        localStorage.setItem('theme', 'light');
+    }
+    else {document.documentElement.setAttribute('data-theme', 'dark');
+          localStorage.setItem('theme', 'dark');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
