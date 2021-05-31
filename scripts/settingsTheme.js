cbf847c0133306d0d88a8c30f36a86904947a353
Joshua <63396516+theradongkid2@users.noreply.github.com>
2021-05-31 22:40:02 +1000

    Extra Fixes to Dark Mode

--- scripts/settingsTheme.js
+++ scripts/settingsTheme.js
@@ -5,8 +5,8 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
-    if (currentTheme === 'light') {
-        toggleSwitch.checked = true;
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = false;
     }
 }
 
